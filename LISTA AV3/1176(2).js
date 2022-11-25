ec77efc1d96a950c0c089163cb64960641f38658
Mariana Vieira <114255481+MarianaV11@users.noreply.github.com>
2022-11-25 00:48:11 -0300

    FIBONACCI ARRAY (2) - FASTER THAN FIRST!

---
+++ LISTA AV3/1176(2).js
@@ -0,0 +1,23 @@
+var prompt = function(texto){return lines.shift();};
+var T = parseInt(prompt());
+
+var Fib = [];
+var TermOne = 0;
+var TermTwo = 1;
+
+Fib.push(TermOne,TermTwo);
+
+for (var i = 0; i < T; i++) {
+    var N = parseInt(prompt());
+    if ((Fib[N]) == null) {
+        for (var y = 0; y < N; y++) {
+            var soma = TermOne + TermTwo;
+            Fib.push(soma);
+            TermOne = TermTwo;
+            TermTwo = soma;
+        }
+        console.log('Fib('+N+') = '+Fib[N]);
+    } else {
+        console.log('Fib('+N+') = '+Fib[N]);
+    }
+}
