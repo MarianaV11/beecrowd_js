742ddf426555023bb646dcc7391d5b715eeb7fb8
Mariana Vieira <114255481+MarianaV11@users.noreply.github.com>
2022-12-01 15:14:54 -0300

    COLUMN IN ARRAY
    It's really close to 1181 exercise. The only change it' is they want a sum or average from column, not line.

---
+++ LISTA AV3/1182.js
@@ -0,0 +1,25 @@
+var prompt = function(texto){return lines.shift();};
+var Colum = parseInt(prompt());
+var option = prompt();
+
+var M = Array (12);
+var sum = 0;
+var i = 0;
+
+for (var L = 0; L < M.length; L++) {
+    M[L] = Array (12);
+    for (var C = 0; C < M.length; C++) {
+        M[L][C] = parseFloat(prompt());
+    }
+}
+
+while (i < 12) {
+    sum = sum + M[i][Colum];
+    i++;
+}
+
+if (option == 'S') {
+    console.log(sum);
+} else {
+    console.log((sum/12).toFixed(1));
+}
