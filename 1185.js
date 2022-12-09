48427b054d4517d4e81d8007a2e287013930e5f4
Mariana Vieira <114255481+MarianaV11@users.noreply.github.com>
2022-12-08 22:02:12 -0300

    ABOVE THE SECUNDARY DIAGONAL

---
+++ 1185.js
@@ -0,0 +1,26 @@
+var prompt = function(texto){return lines.shift();};
+var M = Array (12);
+var Sum = 0;
+var aux = 10;
+
+var option = prompt();
+
+for (var L = 0; L < 12; L++) {
+    M[L] = Array (12);
+    for (var C = 0; C < 12; C++) {
+        M[L][C] = parseFloat(prompt());
+    }
+}
+
+for (L = 0; L < 12; L++) {
+    for (C = 0; C <= aux; C++) {
+        Sum += M[L][C];
+    }
+    aux--;
+}
+
+if (option == 'S') {
+    console.log(Sum.toFixed(1));
+} else {
+    console.log((Sum/66).toFixed(1));
+}
