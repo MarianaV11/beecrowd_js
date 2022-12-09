66fabc99984aa97cd7f5245bc50caa5b95bd185b
Mariana Vieira <114255481+MarianaV11@users.noreply.github.com>
2022-12-08 22:15:18 -0300

    INFERIOR AREA

---
+++ 1188.js
@@ -0,0 +1,28 @@
+var prompt = function(texto){return lines.shift();};
+var M = Array (12);
+var Sum = 0;
+var aux = 5;
+var aux2 = 6;
+
+var option = prompt();
+
+for (var L = 0; L < 12; L++) {
+    M[L] = Array (12);
+    for (var C = 0; C < 12; C++) {
+        M[L][C] = parseFloat(prompt());
+    }
+}
+
+for (L = 7; L < 12; L++) {
+    for (C = aux; C <= aux2; C++) {
+        Sum += M[L][C];
+    }
+    aux--;
+    aux2++;
+}
+
+if (option == 'S') {
+    console.log(Sum.toFixed(1));
+} else {
+    console.log((Sum/30).toFixed(1));
+}
